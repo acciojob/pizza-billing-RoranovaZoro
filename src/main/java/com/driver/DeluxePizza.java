dc3eb228fdd6c2d41245866575463acb063f0335
RoranovaZoro <kashdhee@gmail.com>
2023-08-26 13:19:58 +0530

    Attempt2

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,7 +5,5 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg)
     {
         super(isVeg);
-        addExtraCheese(); // Deluxe pizza always has extra cheese
-        addExtraToppings(); // Deluxe pizza always has extra toppings
     }
 }
